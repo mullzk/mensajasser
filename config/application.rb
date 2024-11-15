24a65a32e4c4268508d0b9e27c5a66a32e21b034
Stefan Mueller <mullzk@users.noreply.github.com>
2024-11-15 02:58:17 +0100

    Update to rails 8.0.0

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Mensajasser
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.active_support.to_time_preserves_timezone = :zone
 
     # Configuration for the application, engines, and railties goes here.
     #
