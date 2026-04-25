5a6fbf4d379f1a47cf880e85984c200993071065
Stefan Mueller <mullzk@users.noreply.github.com>
2026-04-25 09:57:15 +0200

    Fix server-config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,6 @@
 # Production deployment — not yet active
 # jasser.mullzk.ch still on old infrastructure
 # set :application_instance, "jasser"
-# set :branch, "production"
-# set :deploy_to, "/var/www/jasser"
-# server "webapps-hetzner-01.mullzk.ch", user: "deploy-app", roles: %w[app db web]
+# set :branch,               "production"
+# set :deploy_to,            "/var/www/jasser"
+# deploy_server_for :production, roles: %w[app db web]
